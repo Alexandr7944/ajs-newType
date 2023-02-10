b97caeedeaa94aa10789842d02855321a66530e0
Alexander Vasnev <a-s-vasnev88@yandex.ru>
2023-02-10 12:30:40 +0300

    add interface imovie

---
+++ src/ts/interface/ibuyable.ts
@@ -0,0 +1,8 @@
+export default interface IBuyable {
+  id: number;
+  name: string;
+  price: number;
+  discount: number;
+  count: number;
+  singleСopy: boolean;
+}
---
+++ src/ts/interface/imovie.ts
@@ -0,0 +1,7 @@
+export default interface IMovie {
+  year: number;
+  country: string;
+  slogan: string;
+  genre: string[];
+  time: string;
+}
